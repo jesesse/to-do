a9ca41f28aef4ba8785fb8f350586a2b8dfb5385
jesse <jesse.j.lamsa@gmail.com>
2022-09-19 20:03:10 +0300

    Etenee jees

--- src/modules/UI.js
+++ src/modules/UI.js
@@ -1,3 +1,4 @@
+import { areIntervalsOverlappingWithOptions } from "date-fns/fp";
 import { app } from "./app";
 
 const UI = (function () {
@@ -13,12 +14,17 @@ const UI = (function () {
 
 
     projectPanel.addEventListener("click", (e) => {
-        if (e.target.className == "project") displayProject(e.target.textContent);
-        if (e.target.className == "remove-project") deleteProject(e.target.textContent);
+        if (e.target.className == "project") displayProject(app.getProject(e.target.textContent));
+        if (e.target.className == "delete-project") app.deleteProject(e.target.previousSibling.textContent);
     });
 
     tabs.addEventListener("click", (e) => {
         if (e.target.className == "tab") displayTasksByDueDate(e.target.textContent);
+    });
+
+    taskView.addEventListener("click", (e) => {
+        if (e.target.className == "task-card") alert(e.target.textContent); // TÄMÄ AVAA TASKI ISOMMAKSI
+        if (e.target.className == "delete-task") alert(e.target.textContent); //TÄMÄ POISTAA TASKIN
     });
 
     addProjectBtn.addEventListener("click", toggleProjectCreationModal);
@@ -27,19 +33,25 @@ const UI = (function () {
     createProjectBtn.addEventListener("click", createProject);
 
 
-    function createTask(){
+
+
+    function createTask() {
         let title = document.getElementById('title').value;
         let priority = document.getElementById('priority').value;
         let dueDate = document.getElementById('dueDate').value;
-        let projectName = header.textContent;
+        let projectName = document.querySelector('.project-header').textContent;
         app.createTask(title, priority, dueDate, projectName);
         toggleTaskCreationModal();
-
     }
 
-    function createProject(){
-        return;
+
+
+    function createProject() {
+        let projectName = document.getElementById('project-name-input').value;
+        app.createProject(projectName);
+        toggleProjectCreationModal();
     }
+
 
     function toggleTaskCreationModal() {
         let addTaskBtn = document.querySelector('.add-task');
@@ -59,29 +71,84 @@ const UI = (function () {
         document.getElementById("project-name-input").value = "";
     }
 
-    function viewProject(project){
-        header.textContent = project.name
+    function displayProject(project) {
+        if (project.name === "Default") header.textContent = header.textContent;
+        else header.textContent = project.name;
+        displayTasks(app.getProjectTasks(project));
     }
 
     function displayTasks(tasks) {
-        console.log(tasks);
+
+        
+
+        while (taskView.lastChild) {
+            taskView.removeChild(taskView.lastChild);
+        }
+
+        if (tasks.length == 0) taskView.textContent = "No tasks... ";
+        
+        for (let i = 0; i < tasks.length; i++) {
+            let newTaskCard = document.createElement('div');
+            let title = document.createElement('div');
+            let priority = document.createElement('div');
+            let dueDate = document.createElement('div');
+            let projectName = document.createElement('div');
+            let deleteTaskBtn = document.createElement('div');
+            
+            title.textContent = tasks[i].title;
+
+            priority.textContent = tasks[i].priority;
+            if (tasks[i].priority == "high") priority.style.backgroundColor = "red";
+            if (tasks[i].priority == "medium") priority.style.backgroundColor = "yellow";
+            if (tasks[i].priority == "low") priority.style.backgroundColor = "green";
+
+            dueDate.textContent = tasks[i].dueDate;
+
+            if (tasks[i].projectName == "Default") projectName.textContent = "";
+            else projectName.textContent = tasks[i].projectName;
+
+            deleteTaskBtn.classList.add('delete-task');
+            newTaskCard.classList.add('task-card');
+
+            newTaskCard.appendChild(title);
+            newTaskCard.appendChild(priority);
+            newTaskCard.appendChild(dueDate);
+            newTaskCard.appendChild(projectName);
+            newTaskCard.appendChild(deleteTaskBtn);
+            
+            taskView.appendChild(newTaskCard);
+        }
     }
 
 
-    function displayTasksByDueDate(dueDate){
+    function displayTasksByDueDate(dueDate) {
         let tasks;
         if (dueDate == "Today") tasks = app.getTodayTasks();
         if (dueDate == "This Week") tasks = app.getThisWeekTasks();
         if (dueDate == "Show All") tasks = app.getAllTasks();
-
         header.textContent = dueDate;
         displayTasks(tasks);
+    }
+
+    function renderProjectPanel() {
+        while (projectPanel.lastChild) projectPanel.removeChild(projectPanel.lastChild)
+        for (let i = 0; i < app.getProjects().length; i++){
+            if (app.getProjects()[i].name === "Default") continue;
+            let projectTab = document.createElement('div')
+            let deleteProjectBtn = document.createElement('div');
+            projectTab.classList.add('project');
+            deleteProjectBtn.classList.add('delete-project');
+            projectTab.textContent = app.getProjects()[i].name;
+            projectTab.appendChild(deleteProjectBtn);
+            projectPanel.appendChild(projectTab);
+        }
     }
 
 
     return {
         displayTasksByDueDate,
-        viewProject
+        displayProject,
+        renderProjectPanel
     }
 
 })();
--- src/modules/app.js
+++ src/modules/app.js
@@ -16,15 +16,31 @@ const app = (function () {
         //This is where there should be loading all the projects from localStorage, and if there is none, then create default Project:
         let defaultProject = Project("Default");
         _projectList.push(defaultProject);
+        UI.renderProjectPanel();
         UI.displayTasksByDueDate("Today");
     }
 
 
+    function getProject(projectName) {
+        return _projectList.find(project => project.name == projectName);
+    }
+
+
+    function getProjects() {
+        return _projectList;    
+    }
+
+
+    function getProjectTasks(project) {
+        return project.getTasks();
+    }
+
+    
     function getTodayTasks() {
         let todayTasks = [];
         _projectList.forEach(project => {
             for (let i = 0; i < project.getTasks().length; i++) {
-                if (isToday((parseISO(dueDate)))) todayTasks.push(project.getTasks()[i])
+                if (isToday((parseISO(project.getTasks()[i].dueDate)))) todayTasks.push(project.getTasks()[i])
             }
         });
         return todayTasks;
@@ -35,7 +51,7 @@ const app = (function () {
         let thisWeekTasks = [];
         _projectList.forEach(project => {
             for (let i = 0; i < project.getTasks().length; i++) {
-                if (isThisWeek((parseISO(dueDate)))) thisWeekTasks.push(project.getTasks()[i])
+                if (isThisWeek((parseISO(project.getTasks()[i].dueDate)))) thisWeekTasks.push(project.getTasks()[i])
             }
         });
         return thisWeekTasks;
@@ -52,9 +68,34 @@ const app = (function () {
         return allTasks;
     }
 
-    function getProject(projectName) {
-        return _projectList.find(project => project.name == projectName);
+
+
+
+
+
+
+    function createProject(projectName) {
+        if (projectName == "") return;
+        let newProject = Project(projectName);
+        _projectList.push(newProject);
+        UI.renderProjectPanel();
+        UI.displayProject(newProject);
     }
+
+
+    function deleteProject(projectName) {
+        let project = getProject(projectName);
+        _projectList.splice(_projectList.indexOf(project), 1);
+        console.log(_projectList)
+        UI.renderProjectPanel();
+        UI.displayTasksByDueDate("Today");
+    }
+
+
+
+
+
+
 
 
     function createTask(title, priority, dueDate, projectName) {
@@ -64,14 +105,25 @@ const app = (function () {
 
         let newTask = Task(title, priority, dueDate, projectName);
         getProject(projectName).addTask(newTask);
+        UI.displayProject(getProject(projectName))
     }
+
+
+
+
+
 
 
     return {
         loadProjects,
+        getProject,
+        getProjects,
+        getProjectTasks,
+        deleteProject,
         getTodayTasks,
         getThisWeekTasks,
         getAllTasks,
+        createProject,
         createTask
     }
 
